da1b600e58a3b3dad3d3ba5e34b424207ce948ae
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-04-19 15:30:49 -0700

    gravity FAIL

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 1200, ForceMode.Impulse);
+        }
+        playerRb.AddForce(Vector3.down * 9.81f, ForceMode.Acceleration);
+
+    }
+}
